fef7a752e0f9456060329d3beabee731f4e85969
Pranee-Crypto <praneetha.bapatla1@gmail.com>
2021-02-28 23:52:55 +0530

    Added first python file

---
+++ Binary_Num_queue.py
@@ -0,0 +1,13 @@
+def Generate_Binary(n):
+    from queue import Queue
+    q=Queue()
+    q.put("1")
+    while(n>0):
+        n-=1
+        s1=q.get()
+        print(s1)
+        s2=s1
+        q.put(s1+"0")
+        q.put(s2+"1")
+n=int(input())
+print(Generate_Binary(n))
